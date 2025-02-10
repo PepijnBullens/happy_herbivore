25abfeddca4a8f62f8ebbda5f97205d03f2137f8
PepijnBullens <pepijnpersoonlijk@gmail.com>
2025-02-10 20:42:41 +0100

    changed some german words

--- code/resources/js/Pages/Welcome/Welcome.jsx
+++ code/resources/js/Pages/Welcome/Welcome.jsx
@@ -82,7 +82,7 @@ export default function Welcome({ images, language = null }) {
                             words={{
                                 english: "Take Away",
                                 dutch: "Afhalen",
-                                german: "Abheben",
+                                german: "Abholen",
                             }}
                         />
                     </PrimaryButton>
